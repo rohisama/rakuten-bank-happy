efa92e909e3c7cc1e1934464c32d4ee2d617144d
rohisama <rohisama@ma-hiro.com>
2019-08-05 00:23:10 +0900

    Update main process.

--- script/main.py
+++ script/main.py
@@ -2,16 +2,23 @@ import time
 from rakuten_keiba import RakutenKeiba
 from chariloto import Chariloto
 from autorace import Autorace
+from oddspark import Oddspark
+from boatrace import Boatrace
 
 def depositting():
     RakutenKeiba.depositting()
     Autorace.depositting()
     Chariloto.depositting()
+    Oddspark.depositting()
+    Boatrace.depositting()
 
 
 def withdrawal():
     Autorace.withdrawal()
     Chariloto.withdrawal()
+    Oddspark.withdrawal()
+    Boatrace.withdrawal()
+    
 
 
 print("start depositting")
